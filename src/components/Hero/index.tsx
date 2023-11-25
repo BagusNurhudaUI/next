e85e5d9902e3df4f46268268cc8378bf89963357
Bagus Nurhuda <bagus.nurhuda@ui.ac.id>
2023-11-25 14:03:37 +0700

    change layout

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -10,41 +10,36 @@ const Hero = () => {
       >
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
-            <div className="w-full px-2 md:px-10 flex flex-col md:flex-row">
-              <div className="md:w-3/5 px-4">
+            <div className="w-full px-2 lg:px-10 flex flex-col md:flex-row">
+              <div className="md:w-3/5 px-4 flex flex-col justify-center items-center">
                 {/* Content */}
                 <div
-                  className="wow fadeInUp mx-auto max-w-[800px] text-center"
+                  className="wow fadeInUp mx-auto max-w-[800px] text-left md:text-center "
                   data-wow-delay=".2s"
                 >
-                  <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                    Free and Open-Source Next.js Template for Startup & SaaS
+                  <h1 className="mb-2 md:mb-5  text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-4xl md:leading-tight">
+                    Hi, I'm Bagus Nurhuda!
                   </h1>
-                  <p className="dark:text-body-color-dark mb-12 text-base !leading-relaxed text-body-color sm:text-lg md:text-xl">
-                    Startup is a free Next.js template for startups and SaaS
-                    business websites that comes with all the essential pages,
-                    components, and sections you need to launch a complete
-                    business website, built with Next.js 13.x and Tailwind CSS.
+                  <p className="dark:text-body-color-dark mb-6 md:mb-12 text-base !leading-relaxed text-body-color sm:text-lg md:text-xl">
+                    I'm a fresh graduate Computer Enginering from University of
+                    Indonesia. I'm passionate about backend development and
+                    software development with 2 years experience with a strong
+                    foundation in programming, database management, and system
+                    design analysis.
                   </p>
-                  <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                  <div className="md:flex flex-col items-left md:justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                     <Link
-                      href="https://nextjstemplates.com/templates/saas-starter-startup"
-                      className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+                      href="https://drive.google.com/file/d/19D7LqtDR9Mcr7C0e2NrN1R4vPmEQsD-D/view"
+                      className="rounded-sm bg-primary px-6 py-3 md:px-8 md:py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                     >
-                      🔥 Get Pro
-                    </Link>
-                    <Link
-                      href="https://github.com/NextJSTemplates/startup-nextjs"
-                      className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
-                    >
-                      Star on GitHub
+                      🔥 Download My CV
                     </Link>
                   </div>
                 </div>
               </div>
-              <div className="md:w-2/5 px-4 ">
+              <div className="md:w-2/5 px-0 md:px-4 ">
                 {/* Image */}
-                <div className="wow fadeInUp absolute bottom-0 right-0 relative mx-auto aspect-[2690/3689] max-w-[500px] max-h-[450px] lg:ml-20">
+                <div className="wow fadeInUp relative mr-0 md:mr-auto mx-auto aspect-[2690/3689] max-w-[500px] max-h-[450px]">
                   <Image
                     src="/images/hero/prof1.png"
                     alt="hero-image"
